d744b7883f8a5d1b5274f11fe6ec2bb0b52dbc83
csims314@gmail.com <chris@createaloop.org>
2022-03-24 14:14:45 -0500

    detail mask and pbr scenes started

---
+++ Assets/Scenes/Complex Scenes/Resources/_Scripts/SetMaterialProperties.cs
@@ -0,0 +1,48 @@
+using UnityEngine; 
+using UnityEditor;
+
+[InitializeOnLoad, ExecuteInEditMode]
+public class SetMaterialProperties : MonoBehaviour
+{
+    public Texture detailTexture;
+    public float detailStep;
+    public Vector2 detailScale;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        SetProperties();
+    }
+
+    void Update()
+    {
+        SetProperties(); // this is dumb, but the script keeps turning off
+    }
+
+    void Awake()
+    {
+        SetProperties();
+    }
+
+    void OnValidate()
+    {
+        SetProperties();
+    }
+
+    void SetProperties()
+    {
+        var rndr = GetComponent<Renderer>();
+
+        var propertyBlock = new MaterialPropertyBlock();
+        rndr.GetPropertyBlock(propertyBlock);
+
+        if (detailTexture != null)
+        {
+            propertyBlock.SetTexture("_DetailTexture", detailTexture);
+            propertyBlock.SetFloat("_DetailStep", detailStep);
+            propertyBlock.SetVector("_DetailScale", detailScale);
+        }
+
+        rndr.SetPropertyBlock(propertyBlock);
+    }
+}
