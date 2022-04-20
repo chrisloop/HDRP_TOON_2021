64c0d550fc53183fa67f14effdbac26cbd6dbe6d
csims314@gmail.com <chris@createaloop.org>
2022-04-20 10:33:26 -0500

    added custom pass shaders to project settings to make sure building the project works, only tested on windows

--- Assets/Scenes/Complex Scenes/Resources/_Scripts/SetMaterialProperties.cs
+++ Assets/Scenes/Complex Scenes/Resources/_Scripts/SetMaterialProperties.cs
@@ -1,7 +1,7 @@
 using UnityEngine; 
 using UnityEditor;
 
-[InitializeOnLoad, ExecuteInEditMode]
+[ExecuteInEditMode]
 public class SetMaterialProperties : MonoBehaviour
 {
     public Texture detailTexture;
